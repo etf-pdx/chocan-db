f47d62cefe6aff5f9d4d80b35ea35ca23ce0ddc8
meechew <cbunt@pdx.edu>
2019-11-13 05:06:33 -0800

    Some development on the DB

--- ChocAnDB.cpp
+++ ChocAnDB.cpp
@@ -1,5 +1,5 @@
 //
-// Created by veteran on 11/8/2019.
+// Created by CBunt on 11/8/2019.
 //
 
 #include "ChocAnDB.h"
@@ -21,3 +21,39 @@ int ChocAnDB::OpenDB()
 {
     squlite3_open(file, &DB);
 }
+
+char *MembTB = "create table MEMBER\n"
+               "(\n"
+               "    ID      INT\n"
+               "        primary key,\n"
+               "    NAME    TEXT,\n"
+               "    ADDRESS TEXT,\n"
+               "    CITY    TEXT,\n"
+               "    STATE   char(2),\n"
+               "    ZIP     INT\n"
+               ")";
+
+
+char *PrvdTB = "create table PROVIDER\n"
+               "(\n"
+               "    ID      INT\n"
+               "        primary key,\n"
+               "    NAME    TEXT,\n"
+               "    ADDRESS TEXT,\n"
+               "    CITY    TEXT,\n"
+               "    STATE   char(2),\n"
+               "    ZIP     INT\n"
+               ")";
+
+
+char *ServTB = "create table SERVICE\n"
+               "(\n"
+               "    SERVICE_PROVIDED DATE,\n"
+               "    SERVICE_LOGGED   DATE,\n"
+               "    SERVICE_NAME     DATE,\n"
+               "    PROVIDER_ID      INT,\n"
+               "    MEMBER_NAME      TEXT,\n"
+               "    MEMBER_ID        INT\n"
+               "        references MEMBER,\n"
+               "    FEE              DECIMAL\n"
+               ")"
