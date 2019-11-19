b7b41d43a9316af288d85c4aeb1b135b377a4756
Long Vo <volon@pdx.edu>
2019-11-19 13:17:41 -0800

    VS2019 SQL Header

--- test/UnitTest.cpp
+++ test/UnitTest.cpp
@@ -11,10 +11,6 @@ int main()
 	int BadResult = 0;
 	int GoodResult = 0;
 
-	// Getting a Linker Error
-	/*
-	 * UnitTest.obj : error LNK2019: unresolved external symbol "public: __thiscall Service::Service(char *,int,char *,char *,int,int,float,char *)" (??0Service@@QAE@PADH00HHM0@Z) referenced in function _main
-	 */
 	Service* GoodData = new Service("Name", 1234567, "09-12-2019", "09-12-2019", 123456789, 987654321, 999.99, "Comments");
 	Service* BadData = new Service("This is a really long name" , 0, "Wrong format date", "Wrong format date", 0, 0, 0.0, "Comments");
 
@@ -24,7 +20,7 @@ int main()
 
 
 	// Step 3: Assert
-	assert(BadResult == 0);		// Should error out
+	assert(BadResult == -1);	// Should error out
 	assert(GoodResult == 0);	// Should be fine
 
 	return 0;
