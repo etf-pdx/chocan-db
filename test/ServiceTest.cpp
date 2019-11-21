3341b985156c19f6f1b65974edf1478f9853e619
Long Vo <volon@pdx.edu>
2019-11-20 19:32:31 -0800

    Just some

--- test/ServiceTest.cpp
+++ test/ServiceTest.cpp
@@ -1,30 +1,18 @@
 #include "ServiceTest.h"
 
-int ServiceTest::DoTest(Service* TestData)
-{
-	if (!TestData)
-		return -1;
-
-	return 0;
-}
-
 int ServiceTest::StartTest()
 {
 	// Step 1: Arrange
-	int BadResult = 0;
-	int GoodResult = 0;
-
 	Service* GoodData = new Service("Name", 1234567, "09-12-2019", "09-12-2019", 123456789, 987654321, 999.99, "Comments");
 	Service* BadData = new Service("This is a really long name", 0, "Wrong format date", "Wrong format date", 0, 0, 0.0, "Comments");
+	Service* EmptyData = NULL;
 
 	// Step 2: Act
-	GoodResult = DoTest(GoodData);
-	BadResult = DoTest(BadData);
 
 
 	// Step 3: Assert
-	assert(BadResult == -1);	// Should error out
-	assert(GoodResult == 0);	// Should be fine
+	assert(EmptyData == NULL);
+	assert(GoodData == NULL);
 
 	delete[] GoodData;
 	delete[] BadData;
