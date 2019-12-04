cd8ef7b74e91d13d9db51804b1b4ea840b69d04a
trent <trent234@gmail.com>
2019-12-04 15:46:17 -0800

    breaking monolith main into functions. all functionality kept the same. ready for unit tests now.

--- src/ChocAnMain.cpp
+++ src/ChocAnMain.cpp
@@ -20,17 +20,29 @@ int logService(Provider & currProv);            // @ Provider terminal
 void displayProviderDirectory();                // Services, service codes and fees
 bool yesorno();                                 // Obtain valid response to yes/no question
 bool dateFormatCheck(string input_date);        //make sure user inputs a well formatted date
+int getUserID();								//seperating code into smaller functions for unit testing. functionality unchanged
+int managerMenu(int ID);						//seperating code into smaller functions for unit testing. functionality unchanged
+int providerMenu(int ID);						//seperating code into smaller functions for unit testing. functionality unchanged
 
 int main()
 {
-	int ID, repID;              // User's ID number, report request ID number
-	bool mgr = false;           // True if user is a manager
-	char choice;                // User's menu selection
-	string user_input;			// tsw- takes user's input before conv to other type (safer)
-	//ChocAnDB db;				// tsw- the db obj 
+	int ID;		                // User's ID number, report request ID number
 
 	// Welcome the user
 	cout << "\n\tWelcome to Chocaholics Anonymous.\n";
+	ID = getUserID();
+
+	if (ID <= MAX_MANAGER)
+		managerMenu(ID);
+	else 
+		providerMenu(ID);
+
+	return 0;
+}
+
+int getUserID() {
+	int ID;
+	string user_input;			// tsw- takes user's input before conv to other type (safer)
 
 	// Obtain a valid ID number:
 	ID = -1;
@@ -52,151 +64,152 @@ int main()
 		if (ID < MIN_MANAGER || ID > MAX_PROVIDER)
 			cout << "\nInput is not a valid ID number.\n";
 	}
+	return ID;
+}
 
-	if (ID <= MAX_MANAGER) {
-		mgr = true;
-		//Manager current_manager;
-	}
+int managerMenu(int ID) {
+	int repID;
+	char choice;                // User's menu selection
+	string user_input;
+	Manager* CurrentManager = new Manager(ID);
+	cout << "\nMANAGER TERMINAL: ACCESS GRANTED\n\n";
+	do {
+		cout << "You can:\n";
+		cout << "\tGenerate a member report\t(Enter 'M')\n";
+		cout << "\tGenerate a provider report\t(Enter 'P')\n";
+		cout << "\tGenerate a summary report\t(Enter 'S')\n";
+		cout << "\tEnter Interactive Mode\t\t(Enter 'I')\n";
+		cout << "\tExit the terminal\t\t\t(Enter 'X')\n";
 
-	//  SIMULATE PROVIDER TERMINAL  //
-	if (!mgr)
-	{
-        Provider* CurrentProvider = new Provider(ID);
-        cout << "\nPROVIDER TERMINAL: ACCESS GRANTED\n\n";
 		do {
-			// Display the menu:
-			cout << "You can:\n";
-			cout << "\tCheck in a member\t\t\t(Enter 'M')\n";
-			cout << "\tLog a service\t\t\t\t(Enter 'S')\n";
-			cout << "\tView the provider directory\t(Enter 'D')\n";
-			cout << "\tExit the terminal\t\t\t(Enter 'X')\n";
-
-			// Get the user's choice & Validate the user's choice:
-			// tsw- sanitizing so inputs > 1 char len will be rejected 
-			// and wont accept just first char as input
+			//make sure its len = 1
 			do {
-				//make sure its len = 1
-				do {
-					cin >> user_input;
-					cin.ignore(INPUT_BUFFER, '\n');
-					if (strlen(user_input.c_str()) != 1) {
-						cout << "Input is not 1 char in length (and it should be)\n";
-					}
-				} while (strlen(user_input.c_str()) != 1);
-				choice = toupper(user_input[0]);
-				//make sure its one of the valid choices
-				if (choice != 'M' && choice != 'S'
-					&& choice != 'D' && choice != 'X') {
-					cout << "Please select from the above options.\n";
+				cin >> user_input;
+				cin.ignore(INPUT_BUFFER, '\n');
+				if (strlen(user_input.c_str()) != 1) {
+					cout << "Input is not 1 char in length (and it should be)\n";
 				}
-			} while (choice != 'M' && choice != 'S'
-				&& choice != 'D' && choice != 'X');
+			} while (strlen(user_input.c_str()) != 1);
+			choice = toupper(user_input[0]);
+			//make sure its one of the valid choices
+			if (choice != 'M' && choice != 'P'
+				&& choice != 'S' && choice != 'I') {
+				cout << "Please select from the above options.\n";
+			}
+		} while (choice != 'M' && choice != 'P'
+			&& choice != 'S' && choice != 'I' && choice != 'X');
 
-			// Call the appropriate function:
-			if (choice == 'M') {    // Check in a member
-				cout << "Placeholder.. M selected.\n";
-				if (!checkInMember(*CurrentProvider)) continue;
+		if (choice == 'M') {    // Member report
+			// Obtain a valid member ID number:
+			repID = -1;
+			while (repID <= MAX_PROVIDER || ID > MAX_ID) {
+				cout << "\nPlease enter the member's nine digit I.D. number.\n";
+				cin >> user_input;
+				cin.ignore(INPUT_BUFFER, '\n');
+				try {
+					repID = stoi(user_input);
+				}
+				catch (invalid_argument e) {
+					cout << "Invalid input\n";
+					repID = -1;
+				}
+				catch (out_of_range e) {
+					cout << "\nInput is out of int's range.\n";
+					repID = -1;
+				}
+				if (repID <= MAX_PROVIDER || ID > MAX_ID)
+					cout << "\nInput is not a valid member ID number.\n";
 			}
-			if (choice == 'S') {    // Log a service
-				cout << "Placeholder.. S selected.\n";
-				if (!logService(*CurrentProvider)) continue;
+			Member* subject = new Member(repID);
+			if (!subject->Get_Report(repID))
+				cout << "\nFailed to generate member report\n";
+		}
+		if (choice == 'P') {    // Provider report
+			// Obtain a valid provider ID number:
+			repID = -1;
+			while (repID <= MAX_MANAGER || ID > MAX_PROVIDER) {
+				cout << "\nPlease enter the provider's nine digit I.D. number.\n";
+				cin >> user_input;
+				cin.ignore(INPUT_BUFFER, '\n');
+				try {
+					repID = stoi(user_input);
+				}
+				catch (invalid_argument e) {
+					cout << "Invalid input\n";
+					repID = -1;
+				}
+				catch (out_of_range e) {
+					cout << "\nInput is out of int's range.\n";
+					repID = -1;
+				}
+				if (repID <= MAX_MANAGER || ID > MAX_PROVIDER)
+					cout << "\nInput is not a valid provider ID number.\n";
 			}
-			if (choice == 'D') {    // Provider directory
-				cout << "Placeholder.. D selected.\n";
-				displayProviderDirectory();
-			}
-		} while (choice != 'X');
-	}
-	//  SIMULATE MANAGER TERMINAL   //
-	else
-	{
-		Manager* CurrentManager = new Manager(ID);
-		cout << "\nMANAGER TERMINAL: ACCESS GRANTED\n\n";
+			Provider* subject = new Provider(repID);
+			if (!subject->Get_Report(repID))
+				cout << "\nFailed to generate provider report\n";
+		}
+		if (choice == 'S') {    // Summary report
+			// TODO: Fetch, display, write summary report
+		}
+		if (choice == 'I') {    // Interactive mode
+			if (CurrentManager->EnterInteractiveMode() < 0)
+				cout << "\nInteractive mode not available\n";
+		}
+	} while (choice != 'X');
+
+	return 0;
+}
+
+int providerMenu(int ID) {
+	char choice;                // User's menu selection
+	string user_input;			// tsw- takes user's input before conv to other type (safer)
+
+	Provider* CurrentProvider = new Provider(ID);
+	cout << "\nPROVIDER TERMINAL: ACCESS GRANTED\n\n";
+	do {
+		// Display the menu:
+		cout << "You can:\n";
+		cout << "\tCheck in a member\t\t\t(Enter 'M')\n";
+		cout << "\tLog a service\t\t\t\t(Enter 'S')\n";
+		cout << "\tView the provider directory\t(Enter 'D')\n";
+		cout << "\tExit the terminal\t\t\t(Enter 'X')\n";
+
+		// Get the user's choice & Validate the user's choice:
+		// tsw- sanitizing so inputs > 1 char len will be rejected 
+		// and wont accept just first char as input
 		do {
-			cout << "You can:\n";
-			cout << "\tGenerate a member report\t(Enter 'M')\n";
-			cout << "\tGenerate a provider report\t(Enter 'P')\n";
-			cout << "\tGenerate a summary report\t(Enter 'S')\n";
-			cout << "\tEnter Interactive Mode\t\t(Enter 'I')\n";
-			cout << "\tExit the terminal\t\t\t(Enter 'X')\n";
-
+			//make sure its len = 1
 			do {
-				//make sure its len = 1
-				do {
-					cin >> user_input;
-					cin.ignore(INPUT_BUFFER, '\n');
-					if (strlen(user_input.c_str()) != 1) {
-						cout << "Input is not 1 char in length (and it should be)\n";
-					}
-				} while (strlen(user_input.c_str()) != 1);
-				choice = toupper(user_input[0]);
-				//make sure its one of the valid choices
-				if (choice != 'M' && choice != 'P'
-					&& choice != 'S' && choice != 'I') {
-					cout << "Please select from the above options.\n";
+				cin >> user_input;
+				cin.ignore(INPUT_BUFFER, '\n');
+				if (strlen(user_input.c_str()) != 1) {
+					cout << "Input is not 1 char in length (and it should be)\n";
 				}
-			} while (choice != 'M' && choice != 'P'
-				&& choice != 'S' && choice != 'I' && choice != 'X');
+			} while (strlen(user_input.c_str()) != 1);
+			choice = toupper(user_input[0]);
+			//make sure its one of the valid choices
+			if (choice != 'M' && choice != 'S'
+				&& choice != 'D' && choice != 'X') {
+				cout << "Please select from the above options.\n";
+			}
+		} while (choice != 'M' && choice != 'S'
+			&& choice != 'D' && choice != 'X');
 
-			if (choice == 'M') {    // Member report
-                // Obtain a valid member ID number:
-                repID = -1;
-                while (repID <= MAX_PROVIDER || ID > MAX_ID) {
-                    cout << "\nPlease enter the member's nine digit I.D. number.\n";
-                    cin >> user_input;
-                    cin.ignore(INPUT_BUFFER, '\n');
-                    try {
-                        repID = stoi(user_input);
-                    }
-                    catch (invalid_argument e) {
-                        cout << "Invalid input\n";
-                        repID = -1;
-                    }
-                    catch (out_of_range e) {
-                        cout << "\nInput is out of int's range.\n";
-                        repID = -1;
-                    }
-                    if (repID <= MAX_PROVIDER || ID > MAX_ID)
-                        cout << "\nInput is not a valid member ID number.\n";
-                }
-                Member* subject = new Member(repID);
-                if (!subject->Get_Report(repID))
-                    cout << "\nFailed to generate member report\n";
-			}
-			if (choice == 'P') {    // Provider report
-                // Obtain a valid provider ID number:
-                repID = -1;
-                while (repID <= MAX_MANAGER || ID > MAX_PROVIDER) {
-                    cout << "\nPlease enter the provider's nine digit I.D. number.\n";
-                    cin >> user_input;
-                    cin.ignore(INPUT_BUFFER, '\n');
-                    try {
-                        repID = stoi(user_input);
-                    }
-                    catch (invalid_argument e) {
-                        cout << "Invalid input\n";
-                        repID = -1;
-                    }
-                    catch (out_of_range e) {
-                        cout << "\nInput is out of int's range.\n";
-                        repID = -1;
-                    }
-                    if (repID <= MAX_MANAGER || ID > MAX_PROVIDER)
-                        cout << "\nInput is not a valid provider ID number.\n";
-                }
-                Provider* subject = new Provider(repID);
-                if (!subject->Get_Report(repID))
-                    cout << "\nFailed to generate provider report\n";
-			}
-			if (choice == 'S') {    // Summary report
-			    // TODO: Fetch, display, write summary report
-			}
-			if (choice == 'I') {    // Interactive mode
-				if (CurrentManager->EnterInteractiveMode() < 0)
-					cout << "\nInteractive mode not available\n";
-			}
-		} while (choice != 'X');
-	}
+		// Call the appropriate function:
+		if (choice == 'M') {    // Check in a member
+			cout << "Placeholder.. M selected.\n";
+			if (!checkInMember(*CurrentProvider)) continue;
+		}
+		if (choice == 'S') {    // Log a service
+			cout << "Placeholder.. S selected.\n";
+			if (!logService(*CurrentProvider)) continue;
+		}
+		if (choice == 'D') {    // Provider directory
+			cout << "Placeholder.. D selected.\n";
+			displayProviderDirectory();
+		}
+	} while (choice != 'X');
 	return 0;
 }
 
